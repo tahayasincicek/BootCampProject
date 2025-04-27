33c83f7fe8c7fcf24feeb216318fc37426f681b9
Taha Yasin Çiçek <116304917+tahayasincicek@users.noreply.github.com>
2025-04-27 22:30:16 +0300

    Add files via upload

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        Task<List<User>> GetAllAsync();
+        Task<User> GetByIdAsync(int id);
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(User user);
+    }
+}
---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,43 @@
+﻿using Business.Abstract;
+using Entities;
+using Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserManager(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<List<User>> GetAllAsync()
+        {
+            return await _userRepository.GetAllAsync();
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            return await _userRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _userRepository.AddAsync(user);
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            await _userRepository.UpdateAsync(user);
+        }
+
+        public async Task DeleteAsync(User user)
+        {
+            await _userRepository.DeleteAsync(user);
+        }
+    }
+}
